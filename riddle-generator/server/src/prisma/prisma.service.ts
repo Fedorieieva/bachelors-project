45a316418e9cc252b85ff525a0b48c59340b7be3
unknown <dashafedoreeva5@gmail.com>
2026-04-27 16:21:32 +0300

    increse connection timeout

--- riddle-generator/server/src/prisma/prisma.service.ts
+++ riddle-generator/server/src/prisma/prisma.service.ts
@@ -10,7 +10,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       connectionString: process.env.DATABASE_URL,
       max: 3,
       idleTimeoutMillis: 30000,
-      connectionTimeoutMillis: 2000,
+      connectionTimeoutMillis: 5000,
     });
 
     const adapter = new PrismaPg(pgPool);
