dc5fd601f29dbf6fff7dbe80829dc8396f7065f1
Fedorieieva <dashafedoreeva5@gmail.com>
2026-05-15 23:23:01 +0300

    code smell fix

--- riddle-generator/client/src/middleware.ts
+++ riddle-generator/client/src/middleware.ts
@@ -37,6 +37,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon\\.ico).*)',
+    String.raw`/((?!_next/static|_next/image|favicon\.ico).*)`,
   ],
 };
